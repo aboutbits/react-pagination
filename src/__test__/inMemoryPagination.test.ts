b1c12705b77732a3f311c347fcafd6f40a02ae22
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-07-19 17:27:54 +0200

    add tests for in memory pagination

---
+++ src/__test__/inMemoryPagination.test.ts
@@ -0,0 +1,69 @@
+import { act, renderHook } from '@testing-library/react-hooks'
+import { useSearchAndPaginationInMemory } from '../inMemoryPagination'
+
+test('should initialize pagination', () => {
+  const { result } = renderHook(() => useSearchAndPaginationInMemory())
+
+  expect(result.current.page).toBe(0)
+  expect(result.current.search).toBe('')
+  expect(result.current.page).toBe(0)
+})
+
+test('should change page', () => {
+  const { result } = renderHook(() => useSearchAndPaginationInMemory())
+
+  act(() => {
+    result.current.paginationActions.setPage(2)
+  })
+
+  expect(result.current.page).toBe(2)
+})
+
+test('should change search', () => {
+  const { result } = renderHook(() => useSearchAndPaginationInMemory())
+
+  act(() => {
+    result.current.searchActions.search('Max')
+  })
+
+  expect(result.current.search).toBe('Max')
+})
+
+test('on search change -> page should be reset', () => {
+  const { result } = renderHook(() => useSearchAndPaginationInMemory())
+
+  act(() => {
+    result.current.paginationActions.setPage(2)
+  })
+
+  expect(result.current.page).toBe(2)
+
+  act(() => {
+    result.current.searchActions.search('Max')
+  })
+
+  expect(result.current.search).toBe('Max')
+  expect(result.current.page).toBe(0)
+})
+
+test('clear pagination should reset search and page', () => {
+  const { result } = renderHook(() => useSearchAndPaginationInMemory())
+
+  act(() => {
+    result.current.searchActions.search('Max')
+  })
+
+  act(() => {
+    result.current.paginationActions.setPage(2)
+  })
+
+  expect(result.current.search).toBe('Max')
+  expect(result.current.page).toBe(2)
+
+  act(() => {
+    result.current.searchActions.clear()
+  })
+
+  expect(result.current.search).toBe('')
+  expect(result.current.page).toBe(0)
+})
