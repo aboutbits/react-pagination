2b73abac5f8786fdc65fd466bb3d998874d98ef7
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-07-21 15:45:49 +0200

    extract actions type in its seperate type to support picking just one function

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,12 @@
+export type Actions = {
+  search: (query: string) => void
+  clear: () => void
+  setPage: (page: number) => void
+}
+
 export type UseSearchAndPagination = {
   search: string
   page: number
   size: number
-  actions: {
-    search: (query: string) => void
-    clear: () => void
-    setPage: (page: number) => void
-  }
+  actions: Actions
 }
