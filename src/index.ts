f077a32679c28c4ff7fca7bbeaa5e0a487dedf0c
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-07-21 16:39:15 +0200

    export everything from index file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { useSearchAndPagination as useSearchAndPaginationInMemory } from './inMemoryPagination'
+export { useSearchAndPagination as useSearchAndPaginationNextRouter } from './nextRouterPagination'
+export { useSearchAndPagination as useSearchAndPaginationReactRouter } from './reactRouterPagination'
+
+export * from './types'
